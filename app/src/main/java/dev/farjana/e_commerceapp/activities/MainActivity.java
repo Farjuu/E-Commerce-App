1c5a53622ba261ea73bc68bca7f7327fd0171f37
Farjana <farjanaakter431@gmail.com>
2023-01-29 18:52:51 +0600

    API calling

--- app/src/main/java/dev/farjana/e_commerceapp/activities/MainActivity.java
+++ app/src/main/java/dev/farjana/e_commerceapp/activities/MainActivity.java
@@ -5,7 +5,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -14,6 +24,7 @@ import dev.farjana.e_commerceapp.adapters.ProductAdapter;
 import dev.farjana.e_commerceapp.databinding.ActivityMainBinding;
 import dev.farjana.e_commerceapp.models.Category;
 import dev.farjana.e_commerceapp.models.Product;
+import dev.farjana.e_commerceapp.utils.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,22 +47,75 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void init_slider() {
-        binding.carousel.addData(new CarouselItem("https://img.freepik.com/free-photo/cropped-image-woman-inputting-card-information-key-phone-laptop-while-shopping-online_1423-68.jpg?w=740&t=st=1674913112~exp=1674913712~hmac=1004a08691d5752e828c29784ae6dade925b326e31b0963694252b8b466b7316","Buy 1 Get 1 Free"));
-        binding.carousel.addData(new CarouselItem("https://img.freepik.com/premium-photo/shopping-concept-empty-shopping-cart-laptop-rustic-wooden-background_532405-2683.jpg?w=740","Get 50% Discount"));
-        binding.carousel.addData(new CarouselItem("https://img.freepik.com/free-photo/cropped-image-woman-inputting-card-information-key-phone-laptop-while-shopping-online_1423-68.jpg?w=740&t=st=1674913112~exp=1674913712~hmac=1004a08691d5752e828c29784ae6dade925b326e31b0963694252b8b466b7316","Buy 1 Get 1 Free"));
-        binding.carousel.addData(new CarouselItem("https://img.freepik.com/premium-photo/shopping-concept-empty-shopping-cart-laptop-rustic-wooden-background_532405-2683.jpg?w=740","Get 50% Discount"));
+    private void getCategories(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
+        StringRequest request = new StringRequest(Request.Method.GET, Constants.GET_CATEGORIES_URL, response -> {
+
+            try {
+                JSONObject mainObj = new JSONObject(response);
+                if(mainObj.getString("status").equals("success")){
+                    JSONArray categoryArray = mainObj.getJSONArray("categories");
+                    for(int i = 0; i< categoryArray.length(); i++){
+                        JSONObject object = categoryArray.getJSONObject(i);
+                        Category category = new Category(
+                                object.getString("name"),
+                                Constants.CATEGORIES_IMAGE_URL+object.getString("icon"),
+                                object.getString("color"),
+                                object.getString("brief"),
+                                object.getInt("id")
+
+                        );
+                        categories.add(category);
+                    }
+                    categoryAdapter.notifyDataSetChanged();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+
+        });
+        requestQueue.add(request);
+    }
+
+    private void init_slider() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET,Constants.GET_OFFERS_URL,response-> {
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                if(jsonObject.getString("status").equals("success")){
+                    JSONArray offerArray = jsonObject.getJSONArray("news_infos");
+                    for(int i = 0; i < offerArray.length(); i++){
+                        JSONObject childObj = offerArray.getJSONObject(i);
+                        binding.carousel.addData(
+                                new CarouselItem(
+                                        Constants.NEWS_IMAGE_URL+ childObj.getString("image"),
+                                        childObj.getString("title")));
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {});
+
+        queue.add(request);
+
+       /* binding.carousel.addData(new CarouselItem("https://img.freepik.com/free-photo/cropped-image-woman-inputting-card-information-key-phone-laptop-while-shopping-online_1423-68.jpg?w=740&t=st=1674913112~exp=1674913712~hmac=1004a08691d5752e828c29784ae6dade925b326e31b0963694252b8b466b7316","Buy 1 Get 1 Free"));
+        binding.carousel.addData(new CarouselItem("https://img.freepik.com/premium-photo/shopping-concept-empty-shopping-cart-laptop-rustic-wooden-background_532405-2683.jpg?w=740","Get 50% Discount"));
+        binding.carousel.addData(new CarouselItem("https://img.freepik.com/free-photo/cropped-image-woman-inputting-card-information-key-phone-laptop-while-shopping-online_1423-68.jpg?w=740&t=st=1674913112~exp=1674913712~hmac=1004a08691d5752e828c29784ae6dade925b326e31b0963694252b8b466b7316","Buy 1 Get 1 Free"));
+        binding.carousel.addData(new CarouselItem("https://img.freepik.com/premium-photo/shopping-concept-empty-shopping-cart-laptop-rustic-wooden-background_532405-2683.jpg?w=740","Get 50% Discount"));
+*/
     }
 
     private void init_categories() {
         categories = new ArrayList<>();
-        categories.add(new Category("Soft drink","https://th.bing.com/th/id/OIP.uf7t4hQV2kLHsSrB0QjaBQAAAA?w=131&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7", "#ffaf49","Lemonade",1));
-        categories.add(new Category("Soft drink","https://th.bing.com/th/id/OIP.uf7t4hQV2kLHsSrB0QjaBQAAAA?w=131&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7", "#ffcf49","Lemonade",2));
-        categories.add(new Category("Soft drink","https://th.bing.com/th/id/OIP.uf7t4hQV2kLHsSrB0QjaBQAAAA?w=131&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7", "#ffa649","Lemonade",3));
-        categories.add(new Category("Soft drink","https://th.bing.com/th/id/OIP.uf7t4hQV2kLHsSrB0QjaBQAAAA?w=131&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7", "#ffaa49","Lemonade",4));
-        categories.add(new Category("Soft drink","https://th.bing.com/th/id/OIP.uf7t4hQV2kLHsSrB0QjaBQAAAA?w=131&h=196&c=7&r=0&o=5&dpr=1.5&pid=1.7", "#ffaf58","Lemonade",5));
         categoryAdapter = new CategoryAdapter(this,categories);
+
+        getCategories();
+
         GridLayoutManager layoutManager = new GridLayoutManager(this,4);
         binding.categoriesList.setLayoutManager(layoutManager);
 
@@ -61,20 +125,47 @@ public class MainActivity extends AppCompatActivity {
     private void init_products() {
 
         products = new ArrayList<>();
-        products.add(new Product("T-shirt","https://th.bing.com/th/id/R.2dff5b3a9a41e416f60e2013ff657942?rik=2V8xzX8QffKhuA&pid=ImgRaw&r=0","In stock",250.00,25.0,12,1));
-        products.add(new Product("Jeans","https://th.bing.com/th/id/R.c6ec3a48247dd6f10b5202b6e0abb7a7?rik=h8pBfR2jOFGUXw&pid=ImgRaw&r=0","In stock",230.00,20.0,12,2));
-        products.add(new Product("Shoes","https://th.bing.com/th/id/R.d087bbfc94327056a99ebd247129d7b6?rik=ytl9j5pdCx4oyA&pid=ImgRaw&r=0","In stock",220.00,15.0,12,3));
-        products.add(new Product("Jewelry","https://th.bing.com/th/id/R.4a404e1920a85969037f7410368c92a3?rik=ymmHMqHAXUUaaw&pid=ImgRaw&r=0","In stock",210.00,50.0,12,4));
-
-        products.add(new Product("T-shirt","https://th.bing.com/th/id/R.2dff5b3a9a41e416f60e2013ff657942?rik=2V8xzX8QffKhuA&pid=ImgRaw&r=0","In stock",250.00,25.0,12,1));
-        products.add(new Product("Jeans","https://th.bing.com/th/id/R.c6ec3a48247dd6f10b5202b6e0abb7a7?rik=h8pBfR2jOFGUXw&pid=ImgRaw&r=0","In stock",230.00,20.0,12,2));
-        products.add(new Product("Shoes","https://th.bing.com/th/id/R.d087bbfc94327056a99ebd247129d7b6?rik=ytl9j5pdCx4oyA&pid=ImgRaw&r=0","In stock",220.00,15.0,12,3));
-        products.add(new Product("Jewelry","https://th.bing.com/th/id/R.4a404e1920a85969037f7410368c92a3?rik=ymmHMqHAXUUaaw&pid=ImgRaw&r=0","In stock",210.00,50.0,12,4));
 
         productAdapter = new ProductAdapter(this,products);
+        getProducts();
+
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
         binding.productsList.setLayoutManager(gridLayoutManager);
         binding.productsList.setAdapter(productAdapter);
 
+    }
+
+    private void getProducts(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = Constants.GET_PRODUCTS_URL+ "?count=4";
+        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+
+            try {
+                JSONObject object = new JSONObject(response);
+                if(object.getString("status").equals("success")){
+                    JSONArray array = object.getJSONArray("products");
+                    for(int i = 0; i < array.length(); i++){
+                        JSONObject child = array.getJSONObject(i);
+                        Product product = new Product(
+                                child.getString("name"),
+                                Constants.PRODUCTS_IMAGE_URL+child.getString("image"),
+                                child.getString("status"),
+                                child.getDouble("price"),
+                                child.getDouble("price_discount"),
+                                child.getInt("stock"),
+                                child.getInt("id")
+                        );
+                        products.add(product);
+                    }
+                    productAdapter.notifyDataSetChanged();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }, error -> {
+
+        });
+        requestQueue.add(request);
     }
 }
